4f5304888d5b7c45854ba8bb0e204f6b6d565fdb
Youyu <li867663466@163.com>
2019-12-09 19:00:31 +0800

    新加了DAG类

---
+++ src/compiler/Optimize.java
@@ -0,0 +1,17 @@
+package compiler;
+
+/**
+ * 
+ * 项目名称：Complier
+ * 四元式优化
+ * @author 北溟有鱼。
+ * @time 2019年12月9日下午4:26:17
+ * @version V1.0
+ */
+public class Optimize {
+	
+	public class DAG{
+		
+	}
+	
+}
