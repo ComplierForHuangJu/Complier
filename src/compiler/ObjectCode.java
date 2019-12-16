b68c63686e020604d07cd0b94906958b00892c89
youyu <li867663466@163.com>
2019-12-16 14:07:26 +0800

    目标代码类

---
+++ src/compiler/ObjectCode.java
@@ -0,0 +1,19 @@
+package compiler;
+/**
+ * 
+ * 项目名称：Complier
+ * 目标代码类
+ * @author 北溟有鱼。
+ * @time 2019年12月15日下午8:49:23
+ * @version V1.0
+ */
+public class ObjectCode {
+	/* 操作码 */
+	String op;		
+	/**
+	 * 操作码类型：分为存取、转向、算术运算、逻辑运算
+	 */
+	enum TYPE{
+		
+	}
+}
